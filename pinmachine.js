c85fed139a836c4755ae36ae5d2b424eea971f7a
Jason Huggins <jason@jrandolph.com>
2010-11-16 16:04:37 -0800

    Okay, so that code wasn't so 'dead' afterall.

--- pinmachine.js
+++ pinmachine.js
@@ -9,6 +9,9 @@ var renderer;
 
 var cube, cube2, plane;
 
+var targetRotation = 0;
+var targetRotationOnMouseDown = 0;
+
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
@@ -189,8 +192,54 @@ function init() {
 	renderer.setSize( parseInt(SCREEN_WIDTH*.8), parseInt(SCREEN_HEIGHT*.7));
 	
 	container.appendChild( renderer.domElement );
+        document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+}
+//
+function onDocumentMouseDown( event ) {
+
+    event.preventDefault();
+
+    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+    document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+    document.addEventListener( 'mouseout', onDocumentMouseOut, false );
+
+    mouseXOnMouseDown = event.clientX - windowHalfX;
+    targetRotationOnMouseDown = targetRotation;
 }
 
+function onDocumentMouseMove( event ) {
+
+    mouseX = event.clientX - windowHalfX;
+
+    targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.02;
+}
+
+function onDocumentMouseUp( event ) {
+
+    document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+    document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+    document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+}
+
+function onDocumentMouseOut( event ) {
+
+    document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+    document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+    document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+}
+
+function onDocumentTouchStart( event ) {
+
+    if ( event.touches.length == 1 ) {
+
+        event.preventDefault();
+
+        mouseXOnMouseDown = event.touches[ 0 ].pageX - windowHalfX;
+        targetRotationOnMouseDown = targetRotation;
+
+    }
+}
+//
 animation_stop = 5;
 pin_movements = []
 pin_move_flag = false;
