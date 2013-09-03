91ba9fe20f62457c439a11bdc6b9255163b02119
Jason Huggins <jason@jrandolph.com>
2013-09-02 21:50:14 -0500

    whitespace cleanup: tabs to spaces

--- public/js/THREEx.windowresize.js
+++ public/js/THREEx.windowresize.js
@@ -15,7 +15,7 @@
 //
 
 /** @namespace */
-var THREEx	= THREEx 		|| {};
+var THREEx  = THREEx        || {};
 
 /**
  * Update renderer and camera when the window is resized
@@ -23,23 +23,23 @@ var THREEx	= THREEx 		|| {};
  * @param {Object} renderer the renderer to update
  * @param {Object} Camera the camera to update
 */
-THREEx.WindowResize	= function(renderer, camera){
-	var callback	= function(){
-		// notify the renderer of the size change
-		renderer.setSize( window.innerWidth, window.innerHeight );
-		// update the camera
-		camera.aspect	= window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-	}
-	// bind the resize event
-	window.addEventListener('resize', callback, false);
-	// return .stop() the function to stop watching window resize
-	return {
-		/**
-		 * Stop watching window resize
-		*/
-		stop	: function(){
-			window.removeEventListener('resize', callback);
-		}
-	};
+THREEx.WindowResize = function(renderer, camera){
+    var callback    = function(){
+        // notify the renderer of the size change
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        // update the camera
+        camera.aspect   = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    }
+    // bind the resize event
+    window.addEventListener('resize', callback, false);
+    // return .stop() the function to stop watching window resize
+    return {
+        /**
+         * Stop watching window resize
+        */
+        stop    : function(){
+            window.removeEventListener('resize', callback);
+        }
+    };
 }
